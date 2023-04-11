686c23e7f8ef9eadf000cd1645f6d700e35c28ca
WalterDSU <91654066+WalterDSU@users.noreply.github.com>
2023-04-11 18:29:49 +0800

    Update __init__.py

--- custom_components/ha_352air_M25/__init__.py
+++ custom_components/ha_352air_M25/__init__.py
@@ -1,35 +1,72 @@
-"""The 352Air PM2.5 sensor integration."""
+"""Custom Integration for Air Quality Sensor."""
 
-import asyncio
 import logging
+import socket
 
+
+from homeassistant.helpers.discovery import load_platform
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_registry import async_get_registry
+from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import DOMAIN
-from .sensor import _352AirDataUpdateCoordinator, _352AirSensor
+from .const import DEFAULT_PORT
 
 _LOGGER = logging.getLogger(__name__)
 
+DOMAIN = "352air_m25"
+
+def create_server(host, port):
+    """Create the UDP server"""
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.setsockopt(socket.IPPROTO_IP,
+                     socket.IP_ADD_MEMBERSHIP,
+                     socket.inet_aton(MULTICAST_ADDR) +
+                     socket.inet_aton(host))
+
+    return server
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the 352Air PM2.5 sensor integration."""
-    return True
+    """Set up the Air Quality Sensor platform"""
+    hass.data[DOMAIN] = {}
 
+    for platform in ["sensor"]:
+        load_platform(hass, platform, DOMAIN, {}, config)
+
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up 352Air PM2.5 sensor from a config entry."""
-    mac_addr = entry.data["mac_addr"]
-    entity_registry = await async_get_registry(hass)
-    if entity_registry.async_is_registered(mac_addr):
-        return False
+    """Set up the Air Quality Sensor platform from a config entry"""
+    # Get config data
+    host = entry.data["host"]
+    port = entry.data.get("port", DEFAULT_PORT)
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Create UDP server
+    try:
+        server = create_server(host, port)
+    except Exception as error:
+        _LOGGER.error("Error starting UDP server: %s", error)
+        raise ConfigEntryNotReady
+
+    # Store server in data dict
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN]["server"] = server
+    
+    return True
+    
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry"""
+    # Shutdown UDP server
+    server = hass.data[DOMAIN].get("server")
+    if server:
+        server.close()
 
     return True
+
+
+
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
